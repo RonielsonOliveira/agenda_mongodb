052d8093e610e38016bc6ac4132e2b2bcfcdb0fb
vboxuser <ronielsonoliveira@live.com>
2026-02-06 09:23:21 -0300

    first commit

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,10 @@
-const Contato = require('../models/ContatoModel')
-exports.index = async(req, res) => {
-     const contatos = await Contato.buscaContatos();
-     res.render('index', {contatos});
-     
-}
+const Contato = require("../models/ContatoModel");
+exports.index = async (req, res) => {
+  try {
+    const contatos = await Contato.buscaContatos();
+    res.render("index", { contatos });
+  } catch (err) {
+    console.error("ERRO NA HOME:", err);
+    res.status(500).send("Erro ao carregar a página");
+  }
+};
